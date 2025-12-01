ab743ada722cece9536c0dbeab8328d708216d12
4416 Владислав Андреевич <sklyarvladislavtl@gmail.com>
2025-12-01 22:37:22 +0300

    solved 158A

--- A/158A.py
+++ A/158A.py
@@ -2,13 +2,17 @@ import sys
 n, k = list(map(int, sys.stdin.readline().split()))
 a = list(map(int, sys.stdin.readline().split()))
 res_k = a[k-1]
-res = 0
 if res_k == 0:
+    res = 0
+    i = 0
+    while a[i] > 0:
+        res += 1
+        i += 1
     print(res)
 else:
-    res = len(a[:k])
+    res = k
     i = k
-    while a[i] == res_k and i < n:
+    while i < n and a[i] == res_k:
         res += 1
         i += 1 
     print(res)
